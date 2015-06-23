202aee36f6c3e80fb50a0b846e352ad5a99ecf95
Lawrence <lawrence@betterlife.io>
2015-06-23 11:14:34 +0800

    Add a clean.sql to clean all the DB schema.

---
+++ clean.sql
@@ -0,0 +1,14 @@
+-- Clean the schema
+drop table purchase_order_line;
+drop table sales_order_line;
+drop table expense;
+drop table incoming;
+drop table purchase_order;
+drop table sales_order;
+drop table product;
+drop table payment_method;
+drop table supplier;
+drop table incoming_category;
+drop table incoming_status;
+drop table expense_category;
+drop table expense_status;
