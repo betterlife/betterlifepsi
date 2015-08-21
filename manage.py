2738e3fa99720cec3993c8e173a14f58d5d167c1
Lawrence <lawrence@betterlife.io>
2015-08-21 12:19:44 +0800

    Remove newrelic and add Sentry for exception monitor and automatically issue creating.

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from flask import Flask
 from flask.ext.migrate import Migrate, MigrateCommand
+from raven.contrib.flask import Sentry
 from flask.ext.script import Manager
 # 'python manage.py db migrate'
 #   - generate migration script from current schema version.
@@ -22,6 +23,11 @@ db.init_app(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+# Init Sentry if not in debug mode
+if app.config['DEBUG'] is not True:
+    app.config['SENTRY_DSN'] = 'https://9f6d2c7e33f24b77baba8a3e17a6dcfa:0afc65c1b9124c61adae430fad653718@app.getsentry.com/50555'
+    sentry = Sentry(app)
 
 if __name__ == '__main__':
     manager.run()
--- test/app_test.py
+++ test/app_test.py
@@ -1,7 +1,5 @@
 import unittest
-import tempfile
 
-import os
 import app
 
 
