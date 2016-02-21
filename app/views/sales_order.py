betterlife/betterlifepsi

commit 78b659f69cbaa0631f328caff93e845f2ed5b41f

1. Translate field External Id to Chinese. 2. Adjust all the detail page fields

app/views/sales_order.py
@@ -8,6 +8,7 @@ from flask.ext.babelex import lazy_gettext
 from app.models import Preference, Incoming, Expense, Shipping, ShippingLine, EnumValues
 from app.views import ModelViewWithAccess, DisabledStringField
 from formatter import expenses_formatter, incoming_formatter, shipping_formatter, default_date_formatter
+from views.custom_fields import ReadonlyStringField
 
 
 class SalesOrderLineInlineAdmin(InlineFormAdmin):
@@ -16,9 +17,11 @@ class SalesOrderLineInlineAdmin(InlineFormAdmin):
         unit_price=dict(label=lazy_gettext('Unit Price')),
         quantity=dict(label=lazy_gettext('Quantity')),
         remark=dict(label=lazy_gettext('Remark')),
+        external_id=dict(label=lazy_gettext('External Id')),
     )
 
     def postprocess_form(self, form):
+        form.external_id = DisabledStringField(label=lazy_gettext('External Id'))
         form.retail_price = DisabledStringField(label=lazy_gettext('Retail Price'))
         form.price_discount = DisabledStringField(label=lazy_gettext('Price Discount'))
         form.original_amount = DisabledStringField(label=lazy_gettext('Original Amount'))
@@ -36,11 +39,15 @@ class SalesOrderAdmin(ModelViewWithAccess):
                    'discount_amount', 'order_date', 'incoming', 'expense', 'so_shipping', 'remark')
     # column_filters = ('order_date', 'remark', 'logistic_amount')
 
-    form_columns = ('logistic_amount', 'order_date', 'remark', 'actual_amount', 'original_amount',
-                    'discount_amount', 'lines')
-    form_edit_rules = ('logistic_amount', 'order_date', 'remark', 'actual_amount',
+    form_columns = ('id', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
+                    'original_amount', 'discount_amount', 'lines')
+    form_edit_rules = ('external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
                        'original_amount', 'discount_amount', 'lines')
     form_create_rules = ('logistic_amount', 'order_date', 'remark', 'lines',)
+
+    column_details_list = ('id', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
+                           'original_amount', 'discount_amount', 'incoming', 'expense', 'so_shipping', 'lines',)
+
     column_editable_list = ('remark',)
 
     form_extra_fields = {
@@ -48,6 +55,9 @@ class SalesOrderAdmin(ModelViewWithAccess):
         'original_amount': DisabledStringField(label=lazy_gettext('Original Amount')),
         'discount_amount': DisabledStringField(label=lazy_gettext('Discount Amount'))
     }
+
+    form_overrides = dict(external_id=ReadonlyStringField)
+
     form_args = dict(
         logistic_amount=dict(default=0),
         order_date=dict(default=datetime.now())
@@ -76,6 +86,7 @@ class SalesOrderAdmin(ModelViewWithAccess):
         'expense': lazy_gettext('Related Expense'),
         'so_shipping': lazy_gettext('Related Shipping'),
         'lines': lazy_gettext('Lines'),
+        'external_id': lazy_gettext('External Id'),
     }
 
     def create_form(self, obj=None):

app/views/supplier.py
@@ -2,6 +2,8 @@
 from flask.ext.admin.model import InlineFormAdmin
 from flask.ext.babelex import lazy_gettext
 from app.views import ModelViewWithAccess
+from views.custom_fields import ReadonlyStringField
+
 
 class PaymentMethodLineInlineAdmin(InlineFormAdmin):
     # column_editable_list = ['account_name', 'account_number', 'bank_name', 'bank_branch', 'remark']
@@ -21,7 +23,14 @@ class SupplierAdmin(ModelViewWithAccess):
     inline_models = (PaymentMethodLineInlineAdmin(PaymentMethod),)
     column_editable_list = ['name', 'qq', 'phone', 'contact', 'email', 'website',
                             'whole_sale_req', 'can_mixed_whole_sale', 'remark']
+
+    column_details_list = ['id', 'external_id', 'name', 'qq', 'phone', 'contact', 'email', 'website',
+                           'whole_sale_req', 'can_mixed_whole_sale', 'remark', 'paymentMethods']
+
     column_searchable_list = ('code', 'name')
+
+    form_overrides = dict(external_id=ReadonlyStringField)
+
     # column_filters = column_searchable_list
     column_labels = {
         'id': lazy_gettext('id'),
@@ -36,4 +45,5 @@ class SupplierAdmin(ModelViewWithAccess):
         'can_mixed_whole_sale': lazy_gettext('Can Mixed Whole Sale'),
         'remark': lazy_gettext('Remark'),
         'paymentMethods': lazy_gettext('Payment Methods'),
+        'external_id': lazy_gettext('External Id'),
     }

app/views/enum_values.py
@@ -7,6 +7,9 @@ class EnumValuesAdmin(ModelViewWithAccess):
 
     column_editable_list = ['display']
     column_searchable_list = ['code', 'display']
+
+    column_details_list = ['id', 'code', 'display', 'type', 'type_values']
+
     # column_filters = ('code', 'display',)
     column_labels = {
         'id': lazy_gettext('id'),

app/views/inventory_transaction.py
@@ -40,6 +40,8 @@ class InventoryTransactionAdmin(ModelViewWithAccess):
     form_create_rules = ('type', 'date', 'remark', 'lines',)
     column_editable_list = ('remark',)
 
+    column_details_list = ('id', 'type', 'date', 'total_amount', 'remark', 'lines', 'it_receiving', 'it_shipping',)
+
     column_labels = {
         'id': lazy_gettext('id'),
         'type': lazy_gettext('Inventory Transaction Type'),

app/views/product.py
@@ -34,6 +34,7 @@ class ProductAdmin(ModelViewWithAccess):
         'available_quantity': lazy_gettext('Available Quantity'),
         'in_transit_quantity': lazy_gettext('In Transit Quantity'),
         'need_advice': lazy_gettext('Need Running Advice'),
+        'external_id': lazy_gettext('External Id'),
     }
 
     form_extra_fields = {
@@ -45,7 +46,7 @@ class ProductAdmin(ModelViewWithAccess):
         'supplier': supplier_formatter
     }
 
-    form_overrides = dict(distinguishing_feature=CKTextAreaField, code=ReadonlyStringField)
+    form_overrides = dict(distinguishing_feature=CKTextAreaField, code=ReadonlyStringField, external_id=ReadonlyStringField)
 
     form_args = dict(
         distinguishing_feature=dict(description=lazy_gettext('Distinguishing feature of this product, useful for '
@@ -60,7 +61,7 @@ class ProductAdmin(ModelViewWithAccess):
     column_list = ('id', 'supplier', 'category', 'code', 'name', 'lead_day', 'deliver_day', 'purchase_price',
                    'retail_price', 'available_quantity', 'in_transit_quantity',)
 
-    form_columns = ('category', 'supplier', 'code', 'name', 'lead_day', 'deliver_day', 'purchase_price',
+    form_columns = ('category', 'supplier', 'code', 'name', 'external_id', 'lead_day', 'deliver_day', 'purchase_price',
                     'retail_price', 'available_quantity', 'in_transit_quantity', 'spec_link', 'need_advice',
                     'distinguishing_feature',)
 
@@ -68,6 +69,10 @@ class ProductAdmin(ModelViewWithAccess):
         'category', 'supplier', 'code', 'name', 'lead_day', 'deliver_day',
         'purchase_price', 'retail_price', 'spec_link', 'distinguishing_feature',
     )
+
+    column_details_list = ('id', 'external_id', 'category', 'supplier', 'code', 'name', 'lead_day', 'deliver_day', 'purchase_price',
+                           'retail_price', 'available_quantity', 'in_transit_quantity', 'spec_link', 'need_advice',
+                           'distinguishing_feature',)
 
     def create_form(self, obj=None):
         form = super(ModelViewWithAccess, self).create_form(obj)

app/views/product_category.py
@@ -9,6 +9,9 @@ class ProductCategoryAdmin(ModelViewWithAccess):
     column_searchable_list = ('code', 'name')
     # column_filters = ('code','name')
     column_editable_list = ['code', 'name']
+
+    column_details_list = ['id', 'name', 'code', 'parent_category', 'sub_categories', 'products']
+
     column_labels = {
         'id': lazy_gettext('id'),
         'name': lazy_gettext('Name'),

app/views/purchase_order.py
@@ -50,6 +50,10 @@ class PurchaseOrderAdmin(ModelViewWithAccess, DeleteValidator):
     column_sortable_list = ('id', 'logistic_amount', 'total_amount', ('status', 'status.display'),
                             'goods_amount', 'order_date', ('supplier', 'supplier.id'),)
     form_excluded_columns = ('expenses', 'receivings')
+
+    column_details_list = ('id', 'supplier', 'status', 'logistic_amount', 'order_date', 'goods_amount', 'total_amount',
+                           'remark', 'lines', 'all_expenses', 'all_receivings')
+
     column_labels = {
         'id': lazy_gettext('id'),
         'logistic_amount': lazy_gettext('Logistic Amount'),
@@ -144,14 +148,14 @@ class PurchaseOrderAdmin(ModelViewWithAccess, DeleteValidator):
         form.lines.form.product.kwargs['query_factory'] = partial(Product.supplier_filter, supplier_id)
         # Set option list of status available
         if obj.status.code == const.PO_RECEIVED_STATUS_KEY:
-            form.status.query = [EnumValues.find_one_by_code(const.PO_RECEIVED_STATUS_KEY),]
+            form.status.query = [EnumValues.find_one_by_code(const.PO_RECEIVED_STATUS_KEY), ]
         elif obj.status.code == const.PO_PART_RECEIVED_STATUS_KEY:
-            form.status.query = [EnumValues.find_one_by_code(const.PO_PART_RECEIVED_STATUS_KEY),]
+            form.status.query = [EnumValues.find_one_by_code(const.PO_PART_RECEIVED_STATUS_KEY), ]
         elif obj.status.code == const.PO_ISSUED_STATUS_KEY:
-            form.status.query = [EnumValues.find_one_by_code(const.PO_ISSUED_STATUS_KEY),]
+            form.status.query = [EnumValues.find_one_by_code(const.PO_ISSUED_STATUS_KEY), ]
         elif obj.status.code == const.PO_DRAFT_STATUS_KEY:
             form.status.query = [EnumValues.find_one_by_code(const.PO_ISSUED_STATUS_KEY),
-                                 EnumValues.find_one_by_code(const.PO_DRAFT_STATUS_KEY),]
+                                 EnumValues.find_one_by_code(const.PO_DRAFT_STATUS_KEY), ]
         # Set product query option for old lines(forbid to change product for existing line)
         line_entries = form.lines.entries
         products = Product.supplier_filter(supplier_id).all()
@@ -161,7 +165,7 @@ class PurchaseOrderAdmin(ModelViewWithAccess, DeleteValidator):
 
     def create_form(self, obj=None):
         form = super(ModelView, self).create_form(obj)
-        form.status.query = [EnumValues.find_one_by_code(const.PO_DRAFT_STATUS_KEY),]
+        form.status.query = [EnumValues.find_one_by_code(const.PO_DRAFT_STATUS_KEY), ]
         return form
 
     def on_model_change(self, form, model, is_created):

app/views/receiving.py
@@ -18,6 +18,7 @@ from wtforms.validators import ValidationError
 
 db = AppInfo.get_db()
 
+
 class ReceivingLineInlineAdmin(InlineFormAdmin):
     form_args = dict(
         purchase_order_line=dict(label=lazy_gettext('Purchase Order Line')),
@@ -56,6 +57,9 @@ class ReceivingAdmin(ModelViewWithAccess, DeleteValidator):
         'transient_po': DisabledStringField(label=lazy_gettext('Related Purchase Order')),
         "total_amount": DisabledStringField(label=lazy_gettext('Total Amount')),
     }
+
+    column_details_list = ('id', 'supplier', 'date', 'status', 'total_amount', 'remark', 'lines',
+                           'purchase_order', 'inventory_transaction',)
     form_widget_args = {
         'create_lines': {'default': True},
     }
@@ -127,7 +131,6 @@ class ReceivingAdmin(ModelViewWithAccess, DeleteValidator):
             elif started is True:
                 po.status = EnumValues.find_one_by_code(const.PO_PART_RECEIVED_STATUS_KEY)
             db.session.add(po)
-
 
     def operate_inv_trans_by_recv_status(self, model):
         inv_trans = None
@@ -234,7 +237,7 @@ class ReceivingAdmin(ModelViewWithAccess, DeleteValidator):
 
     def create_form(self, obj=None):
         form = super(ModelView, self).create_form(obj)
-        form.status.query = [EnumValues.find_one_by_code(const.RECEIVING_DRAFT_STATUS_KEY),]
+        form.status.query = [EnumValues.find_one_by_code(const.RECEIVING_DRAFT_STATUS_KEY), ]
         form.create_lines.data = True
         return form
 
@@ -242,10 +245,10 @@ class ReceivingAdmin(ModelViewWithAccess, DeleteValidator):
         form = super(ModelView, self).edit_form(obj)
         po_id = obj.transient_po.id
         # Set query_factory for newly added line
-        form.lines.form.purchase_order_line.kwargs['query_factory'] =\
+        form.lines.form.purchase_order_line.kwargs['query_factory'] = \
             partial(PurchaseOrderLine.header_filter, po_id)
         if obj is not None and obj.status is not None and obj.status.code == const.RECEIVING_COMPLETE_STATUS_KEY:
-            form.status.query = [EnumValues.find_one_by_code(const.RECEIVING_COMPLETE_STATUS_KEY),]
+            form.status.query = [EnumValues.find_one_by_code(const.RECEIVING_COMPLETE_STATUS_KEY), ]
         # Set query option for old lines
         line_entries = form.lines.entries
         po_lines = PurchaseOrderLine.header_filter(po_id).all()

commit 62e4ccd6afbd9ad1414866954a4de1fcbcca8110

Fix build issues introduced by 78b659f69cbaa0631f328caff93e845f2ed5b41f

app/views/sales_order.py
@@ -8,7 +8,7 @@ from flask.ext.babelex import lazy_gettext
 from app.models import Preference, Incoming, Expense, Shipping, ShippingLine, EnumValues
 from app.views import ModelViewWithAccess, DisabledStringField
 from formatter import expenses_formatter, incoming_formatter, shipping_formatter, default_date_formatter
-from views.custom_fields import ReadonlyStringField
+from app.views.custom_fields import ReadonlyStringField
 
 
 class SalesOrderLineInlineAdmin(InlineFormAdmin):

app/views/supplier.py
@@ -2,7 +2,7 @@
 from flask.ext.admin.model import InlineFormAdmin
 from flask.ext.babelex import lazy_gettext
 from app.views import ModelViewWithAccess
-from views.custom_fields import ReadonlyStringField
+from app.views.custom_fields import ReadonlyStringField
 
 
 class PaymentMethodLineInlineAdmin(InlineFormAdmin):