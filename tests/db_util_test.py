betterlife/betterlifepsi

commit 8adeff46f83006a514aa75c8d77885e9212b2d87

Works on BE-120 #time 4h (Customer support)

app/models/customer.py
@@ -1,0 +1,75 @@
+# encoding=utf-8
+
+# encoding: utf-8
+import datetime
+
+from app import const
+from app.app_provider import AppInfo
+from app.models.enum_values import EnumValues
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, String, Date
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import relationship
+from utils import date_util
+
+db = AppInfo.get_db()
+
+
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(32), unique=False, nullable=True)
+    last_name = Column(String(32), unique=False, nullable=True)
+    mobile_phone = Column(String(32), unique=False, nullable=True)
+    email = Column(String(64))
+    address = Column(String(64), unique=False, nullable=True)
+    birthday = Column(Date, nullable=True)
+    join_date = Column(DateTime, nullable=False)
+    points = Column(Integer, nullable=False)
+
+    join_channel_id = Column(Integer, ForeignKey('enum_values.id'), nullable=True)
+    join_channel = relationship('EnumValues', foreign_keys=[join_channel_id])
+
+    level_id = Column(Integer, ForeignKey('enum_values.id'), nullable=False)
+    level = relationship('EnumValues', foreign_keys=[level_id])
+
+    @hybrid_property
+    def member_age(self):
+        return int(date_util.num_years(self.join_date))
+
+    @member_age.setter
+    def member_age(self, val):
+        pass
+
+    @hybrid_property
+    def name(self):
+        connect = ''
+        if str(self.last_name).isalpha() and str(self.first_name).isalpha():
+            connect = ' '
+        return self.last_name + connect + self.first_name
+
+    @name.setter
+    def name(self, value):
+        pass
+
+    @hybrid_property
+    def total_spent(self):
+        orders = self.sales_orders
+        t = 0
+        for order in orders:
+            t += order.actual_amount
+        return t
+
+    @total_spent.setter
+    def total_spent(self, val):
+        pass
+
+    @staticmethod
+    def join_channel_filter():
+        return EnumValues.type_filter(const.CUSTOMER_JOIN_CHANNEL_KEY)
+
+    @staticmethod
+    def level_filter():
+        return EnumValues.type_filter(const.CUSTOMER_LEVEL_KEY)
+
+    def __repr__(self):
+        return self.name + ' - ' + self.level.display

app/views/customer.py
@@ -1,0 +1,57 @@
+# encoding=utf-8
+from datetime import datetime
+
+from app.views import ModelViewWithAccess
+from flask.ext.babelex import lazy_gettext
+from app.views.formatter import default_date_formatter
+from app.models.customer import Customer
+
+
+class CustomerAdmin(ModelViewWithAccess):
+    column_list = ('id', 'name', 'mobile_phone', 'email', 'address', 'birthday',
+                   'join_date', 'member_age', 'join_channel', 'level', 'total_spent')
+
+    column_labels = {
+        'id': lazy_gettext('id'),
+        'name': lazy_gettext('Name'),
+        'mobile_phone': lazy_gettext('Mobile Phone'),
+        'email': lazy_gettext('Email'),
+        'birthday': lazy_gettext('Birthday'),
+        'address': lazy_gettext('Address'),
+        'join_date': lazy_gettext('Join Date'),
+        'member_age': lazy_gettext('Member Year'),
+        'join_channel': lazy_gettext('Join Channel'),
+        'level': lazy_gettext('Customer Level'),
+        'total_spent': lazy_gettext('Total Spent'),
+        'first_name': lazy_gettext('First Name'),
+        'last_name': lazy_gettext('Last Name'),
+        'points': lazy_gettext('Points'),
+        'sales_orders': lazy_gettext('Sales Order')
+    }
+
+    # column_filters = ['member_age', 'total_spent']
+
+    edit_modal = True
+    create_modal = True
+
+    form_excluded_columns = ('sales_orders',)
+
+    column_searchable_list = ['first_name', 'last_name', 'mobile_phone', 'address', 'email']
+
+    column_formatters = {
+        'join_date': default_date_formatter,
+    }
+
+    form_create_rules = form_edit_rules = ['last_name', 'first_name', 'mobile_phone',
+                                           'email', 'address', 'birthday', 'join_channel',
+                                           'join_date', 'level', 'points']
+
+    column_details_list = ['last_name', 'first_name', 'mobile_phone',
+                           'email', 'address', 'birthday', 'join_channel',
+                           'join_date', 'level', 'points', 'sales_orders']
+
+    form_args = dict(
+        join_channel=dict(query_factory=Customer.join_channel_filter),
+        level=dict(query_factory=Customer.level_filter),
+        join_date=dict(default=datetime.now()),
+    )

app/const.py
@@ -30,4 +30,6 @@ INCOMING_STATUS_KEY = u'INCOMING_STATUS'
 # Shipping complete status key
 SHIPPING_COMPLETE_STATUS_KEY = u'SHIPPING_COMPLETE'
 
-
+# Customer related enum value keys
+CUSTOMER_JOIN_CHANNEL_KEY = u'CUSTOMER_JOIN_CHANNEL'
+CUSTOMER_LEVEL_KEY = u'CUSTOMER_LEVEL'

app/models/__init__.py
@@ -7,6 +7,7 @@ from product_category import ProductCategory
 from supplier import Supplier, PaymentMethod
 from product import Product
 from enum_values import EnumValues
+from customer import Customer
 from expense import Expense
 from incoming import Incoming
 from shipping import Shipping, ShippingLine

app/models/sales_order.py
@@ -17,6 +17,10 @@ class SalesOrder(db.Model):
     logistic_amount = Column(Numeric(precision=8, scale=2, decimal_return_scale=2))
     order_date = Column(DateTime, nullable=False)
     external_id = Column(String(), nullable=True)
+
+    customer_id = Column(Integer, ForeignKey('customer.id'), nullable=True)
+    customer = relationship('Customer', foreign_keys=[customer_id], backref=backref('sales_orders', uselist=True))
+
     remark = Column(Text)
 
     @hybrid_property

app/utils/date_util.py
@@ -1,0 +1,24 @@
+# encoding=utf-8
+from datetime import datetime
+
+
+def years_ago(years, from_date=None):
+    if from_date is None:
+        from_date = datetime.now()
+    try:
+        return from_date.replace(year=from_date.year - years)
+    except ValueError:
+        # Must be 2/29!
+        assert from_date.month == 2 and from_date.day == 29
+        return from_date.replace(month=2, day=28,
+                                 year=from_date.year - years)
+
+
+def num_years(begin, end=None):
+    if end is None:
+        end = datetime.now()
+    num_of_years = int((end - begin).days / 365.25)
+    if begin > years_ago(num_of_years, end):
+        return num_of_years - 1
+    else:
+        return num_of_years

app/views/__init__.py
@@ -16,6 +16,7 @@ from receiving import ReceivingAdmin
 from shipping import ShippingAdmin
 from inventory_transaction import InventoryTransactionAdmin
 from product_inventory import ProductInventoryView
+from customer import CustomerAdmin
 from formatter import *
 from app.models import *
 from flask.ext.admin import Admin
@@ -45,9 +46,11 @@ def init_admin_views(app, db):
     adminViews.add_view(IncomingAdmin(Incoming, db_session, name=lazy_gettext("Incoming"),
                                       category=u'财务', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-usd'))
     adminViews.add_view(SupplierAdmin(Supplier, db_session, name=lazy_gettext("Supplier"),
-                                      category=u'主数据', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-user'))
+                                      category=u'主数据', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-globe'))
     adminViews.add_view(ProductAdmin(Product, db_session, name=lazy_gettext("Product"),
                                      category=u'主数据', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-barcode'))
+    adminViews.add_view(CustomerAdmin(Customer, db_session, name=lazy_gettext("Customer"),
+                                      category=u'主数据', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-user'))
     adminViews.add_view(ProductCategoryAdmin(ProductCategory, db_session, name=lazy_gettext("Product Category"),
                                              category=u'主数据', menu_icon_type=ICON_TYPE_GLYPH, menu_icon_value='glyphicon-tags'))
     adminViews.add_view(ImportStoreDataView(name=lazy_gettext("Import Store Data"), category=u'数据维护',

app/views/sales_order.py
@@ -35,17 +35,17 @@ class SalesOrderLineInlineAdmin(InlineFormAdmin):
 class SalesOrderAdmin(ModelViewWithAccess):
     from app.models import SalesOrderLine
 
-    column_list = ('id', 'logistic_amount', 'actual_amount', 'original_amount',
+    column_list = ('id', 'customer', 'logistic_amount', 'actual_amount', 'original_amount',
                    'discount_amount', 'order_date', 'incoming', 'expense', 'so_shipping', 'remark')
     # column_filters = ('order_date', 'remark', 'logistic_amount')
 
-    form_columns = ('id', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
+    form_columns = ('id', 'customer', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
                     'original_amount', 'discount_amount', 'lines')
-    form_edit_rules = ('external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
+    form_edit_rules = ('customer', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
                        'original_amount', 'discount_amount', 'lines')
-    form_create_rules = ('logistic_amount', 'order_date', 'remark', 'lines',)
+    form_create_rules = ('customer', 'logistic_amount', 'order_date', 'remark', 'lines',)
 
-    column_details_list = ('id', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
+    column_details_list = ('id', 'customer', 'external_id', 'logistic_amount', 'order_date', 'remark', 'actual_amount',
                            'original_amount', 'discount_amount', 'incoming', 'expense', 'so_shipping', 'lines',)
 
     column_editable_list = ('remark',)
@@ -87,6 +87,7 @@ class SalesOrderAdmin(ModelViewWithAccess):
         'so_shipping': lazy_gettext('Related Shipping'),
         'lines': lazy_gettext('Lines'),
         'external_id': lazy_gettext('External Id'),
+        'customer': lazy_gettext('Customer'),
     }
 
     def create_form(self, obj=None):

commit 1484552ba5ca071a7dc2e82968e81c7b13f2761b

Works on BE-120 #time 3h (Customer support), fixes test failure introduced by 8adeff4

Procfile
@@ -1,1 +1,1 @@
-web: gunicorn start:app --log-file=- --timeout 300
+web: gunicorn start:start --log-file=- --timeout 300

app/config.py
@@ -12,6 +12,7 @@ SECURITY_CONFIRMABLE = False
 SECURITY_RECOVERABLE = False
 SECURITY_CHANGEABLE = False
 DEBUG = (os.environ.get('DEBUG') == "True")
+TESTING = (os.environ.get('TESTING') == "True")
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 # TODO: Move those business related settings to a table and make it changeable via UI.

app/models/customer.py
@@ -9,7 +9,7 @@ from app.models.enum_values import EnumValues
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, String, Date
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import relationship
-from utils import date_util
+from app.utils import date_util
 
 db = AppInfo.get_db()
 

app/templates/layout.html
@@ -11,6 +11,12 @@
   {{ super() }}
   <script src="{{ url_for('static', filename='ckeditor/ckeditor.js') }}"></script>
   <script src="{{ url_for('static', filename='js/common.js') }}"></script>
+  <div align="center">
+    © 2015-2016 BETTERLIFE.IO
+  </div>
+  <div class='main-footer' align="center">
+    Current Branch: ${SNAP_BRANCH}, Commit: <a href="https://github.com/betterlife/flask-psi/commit/${SNAP_COMMIT}">${SNAP_COMMIT_SHORT}</a>,
+  </div>
 {% endblock %}
 
 {% block access_control %}

start.py
@@ -5,90 +5,81 @@ from flask_security import SQLAlchemyUserDatastore, Security
 from flask.ext.debugtoolbar import DebugToolbarExtension
 from raven.contrib.flask import Sentry
 
-app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
-import app.config as config
+def start(custom_config=None):
+    app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
-app.config.from_object(config)
+    if custom_config is None:
+        import app.config as default_config
+        app.config.from_object(default_config)
+    else:
+        app.config.from_object(custom_config)
 
-from flask_babelex import Babel
+    from flask_babelex import Babel
 
-babel = Babel(default_locale='zh_Hans_CN')
-babel.init_app(app)
+    babel = Babel(default_locale='zh_Hans_CN')
+    babel.init_app(app)
 
-toolbar = DebugToolbarExtension(app)
+    toolbar = DebugToolbarExtension(app)
 
-import os.path as op
-
-import logging
-from logging import StreamHandler
-
-log_handler = StreamHandler()
-app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
-app.logger.addHandler(log_handler)
-
-db = None
-
-
-def init_db_security_admin():
-    from app.app_provider import AppInfo
-    if AppInfo.get_db() is None:
-        from flask_sqlalchemy import SQLAlchemy
-        v_db = SQLAlchemy(app)
-        AppInfo.set_db(v_db)
-        from app.models import User, Role
-        v_db.init_app(app)
-
-        # Setup Flask-Security
-        user_datastore = SQLAlchemyUserDatastore(v_db, User, Role)
-        Security(app, user_datastore)
-
-        from app.views import init_admin_views
-        init_admin_views(app, v_db)
-        return v_db
-    return db
-
-
-if not app.config['TESTING']:
-    db = init_db_security_admin()
-
-# Init Sentry if not in debug mode
-if app.config['DEBUG'] is not True:
-    sentry = Sentry(app)
-else:
     import logging
+    from logging import StreamHandler
 
-    # Disable werkzeug log
-    log = logging.getLogger('werkzeug')
-    log.setLevel(logging.ERROR)
+    log_handler = StreamHandler()
+    app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+    app.logger.addHandler(log_handler)
+
+    from app.app_provider import AppInfo
+    from flask_sqlalchemy import SQLAlchemy
+    db = SQLAlchemy(app)
+    AppInfo.set_db(db)
+    from app.models import User, Role
+    db.init_app(app)
+
+    # Setup Flask-Security
+    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+    Security(app, user_datastore)
+
+    from app.views import init_admin_views
+    init_admin_views(app, db)
+
+    # Init Sentry if not in debug mode
+    if app.config['DEBUG'] is not True:
+        sentry = Sentry(app)
+    else:
+        import logging
+
+        # Disable werkzeug log
+        log = logging.getLogger('werkzeug')
+        log.setLevel(logging.ERROR)
+
+    @app.route('/')
+    def hello():
+        return redirect("/admin", code=302)
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'] or True, use_reloader=False)
+
+    @app.before_request
+    def before_request():
+        pass
+
+    @app.teardown_request
+    def teardown_request(exception):
+        pass
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+    @babel.localeselector
+    def get_locale():
+        # Put your logic here. Application can store locale in
+        # user profile, cookie, session, etc.
+        return 'zh_CN'
+
+    return app
 
 
-@app.route('/')
-def hello():
-    return redirect("/admin", code=302)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=app.config['DEBUG'] or True, use_reloader=False)
-
-
-@app.before_request
-def before_request():
-    pass
-
-
-@app.teardown_request
-def teardown_request(exception):
-    pass
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db.session.remove()
-
-
-@babel.localeselector
-def get_locale():
-    # Put your logic here. Application can store locale in
-    # user profile, cookie, session, etc.
-    return 'zh_CN'
+if not os.environ.get('TESTING'):
+    start()

run_tests.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export TESTING="True"
 pip install -q -r test_requirements.txt
 mkdir -p reports/html
 nosetests -w tests --with-coverage --cover-html  --cover-html-dir=../reports/html --cover-branches

tests/app_test.py
@@ -8,11 +8,11 @@ import os
 class TestCases(unittest.TestCase):
     def setUp(self):
         config.TESTING = True
-        config.DATABASE_URL = (os.environ.get('TEST_DATABASE_URL') or 'postgres://flask_sit:flask_sit@localhost:5432/flask_sit')
+        config.SQLALCHEMY_DATABASE_URI = (os.environ.get('TEST_DATABASE_URL') or 'postgres://flask_sit:flask_sit@localhost:5432/flask_sit')
         config.WTF_CSRF_ENABLED = False
         import start
-        start.db = start.init_db_security_admin()
-        self.test_client = start.app.test_client()
+        application = start.start(config)
+        self.test_client = application.test_client()
 
     def tearDown(self):
         pass

tests/date_util_test.py
@@ -1,0 +1,16 @@
+# encoding=utf-8
+import unittest
+from datetime import datetime, date, time
+
+from utils import date_util
+
+
+class TestCases(unittest.TestCase):
+    def test_num_years(self):
+        begin = datetime.combine(date(2014, 06, 24), time(12, 30))
+        years = date_util.num_years(begin)
+        self.assertEqual(1, years)
+        begin = datetime.combine(date(2000, 06, 24), time(12, 30))
+        self.assertEqual(15, date_util.num_years(begin))
+        end = datetime.combine(date(2016, 06, 24), time(12,30))
+        self.assertEqual(16, date_util.num_years(begin, end), years)

tests/db_util_test.py
@@ -1,0 +1,19 @@
+import unittest
+
+
+class TestCases(unittest.TestCase):
+    def setUp(self):
+        import os
+        import app.config as config
+        config.TESTING = True
+        config.SQLALCHEMY_DATABASE_URI = (os.environ.get('TEST_DATABASE_URL') or 'postgres://flask_sit:flask_sit@localhost:5432/flask_sit')
+        config.WTF_CSRF_ENABLED = False
+        import start
+        application = start.start(config)
+        self.test_client = application.test_client()
+
+    def test_get_next_code(self):
+        from app.models.supplier import Supplier
+        from app.utils import db_util
+        next_code = db_util.get_next_code(Supplier)
+        self.assertEqual('000001', next_code)