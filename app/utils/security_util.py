betterlife/betterlifepsi

commit f111743ba0280d0c332c373fbe56124fd4290421

Clean up business permissions from super admin (BE-217)

app/utils/security_util.py
@@ -1,4 +1,5 @@
 # encoding=utf-8
+from const import SUPER_ADMIN_ROLE_NAME
 from flask.ext.login import current_user
 
 
@@ -14,7 +15,7 @@ def exclude_super_admin_roles(name, query):
         name != 'organization_edit',
         name != 'organization_create',
         name != 'organization_delete',
-        name != 'superadmin')
+        name != SUPER_ADMIN_ROLE_NAME)
 
 
 def is_super_admin(user=current_user):
@@ -23,7 +24,7 @@ def is_super_admin(user=current_user):
     :param user: user to judge
     :return: True if the user is a super admin, otherwise False
     """
-    return user.has_role('superadmin')
+    return user.has_role(SUPER_ADMIN_ROLE_NAME)
 
 
 def get_user_roles(user=current_user):
@@ -49,7 +50,7 @@ def user_has_role(role, user=current_user):
     :param role: Role to check
     :return:True if user has the role, otherwise False
     """
-    user_roles = get_user_roles(current_user)
+    user_roles = get_user_roles(user)
     return role in user_roles
 
 

app/const.py
@@ -37,3 +37,6 @@ CUSTOMER_LEVEL_KEY = u'CUSTOMER_LEVEL'
 # User locale settings
 LANGUAGE_VALUES_KEY = u'LANGUAGES'
 TIMEZONE_VALUES_KEY = u'TIMEZONES'
+
+# Super admin role
+SUPER_ADMIN_ROLE_NAME = u'super_admin'

app/utils/db_util.py
@@ -69,3 +69,12 @@ def delete_by_id(obj_type, id_to_del):
     obj = db.session.query(obj_type).get(id_to_del)
     db.session.delete(obj)
     db.session.commit()
+
+
+def get_first_result_raw_sql(op, sql):
+    res = op.get_bind().execute(sql)
+    results = res.fetchall()
+    result = None
+    for r in results:
+        result = r[0]
+    return result

migrations/versions/19_93a6e0ee63c9_.py
@@ -1,0 +1,41 @@
+""" Add super_admin role and grant base administrator roles to it
+
+Revision ID: 93a6e0ee63c9
+Revises: 8ff0624764b7
+Create Date: 2016-04-18 19:27:39.063915
+
+"""
+
+# revision identifiers, used by Alembic.
+from app.utils import db_util
+
+revision = '93a6e0ee63c9'
+down_revision = '8ff0624764b7'
+
+from alembic import op
+
+
+def upgrade():
+    ### commands auto generated by Alembic - please adjust! ###
+    from sqlalchemy.sql import text
+    op.get_bind().execute(text("INSERT INTO role (name, description) VALUES ('super_admin', 'Super Admin Role for all Organizations');"))
+    op.get_bind().execute(text("UPDATE \"user\" SET login='super_admin' WHERE id=1;"))
+    op.get_bind().execute(text("UPDATE role set name='organization_admin' where name = 'admin'"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = (SELECT id FROM role WHERE name = 'super_admin') WHERE name LIKE '%organization_%';"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = (SELECT id FROM role WHERE name = 'organization_admin') WHERE name LIKE '%user_%';"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = (SELECT id FROM role WHERE name = 'organization_admin') WHERE name LIKE '%role_%';"))
+    op.get_bind().execute(text("INSERT INTO roles_users (role_id, user_id) VALUES ((SELECT id FROM role WHERE name='super_admin'), 1);"))
+    ### end Alembic commands ###
+
+
+def downgrade():
+    ### commands auto generated by Alembic - please adjust! ###
+    from sqlalchemy.sql import text
+    op.get_bind().execute(text("DELETE FROM roles_users WHERE role_id = (SELECT id FROM role WHERE name='super_admin') AND user_id = 1;"))
+    op.get_bind().execute(text("UPDATE \"user\" SET login='admin' WHERE id=1;"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = NULL WHERE name LIKE '%organization_%';"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = NULL WHERE name LIKE '%user_%';"))
+    op.get_bind().execute(text("UPDATE role SET parent_id = NULL WHERE name LIKE '%role_%';"))
+    op.get_bind().execute(text("UPDATE role set name='admin' where name = 'organization_admin'"))
+    op.get_bind().execute(text("DELETE FROM role WHERE name='super_admin';"))
+    ### end Alembic commands ###

commit 5c70836b87209b4ddb21b4cc6d466cc83f1646e4

Fix test failures introduced by f111743

app/utils/security_util.py
@@ -1,5 +1,5 @@
 # encoding=utf-8
-from const import SUPER_ADMIN_ROLE_NAME
+from app.const import SUPER_ADMIN_ROLE_NAME
 from flask.ext.login import current_user
 
 

app/views/import_store_data.py
@@ -14,7 +14,7 @@ from flask.ext.babelex import gettext
 from flask.ext.login import login_required, current_user
 from flask_admin import expose
 from app.utils.decorations import has_role
-from utils.security_util import get_user_roles, user_has_role
+from app.utils.security_util import user_has_role
 
 
 def create_or_update_supplier(sup_num, sup_name):

tests/views/import_store_data_test.py
@@ -25,7 +25,7 @@ class TestCases(unittest.TestCase):
         content = codecs.open(os.path.dirname(os.path.realpath(__file__)) + "/store_data.csv", "r", "utf-8").read()
         from app.models.security import User, Role
         from app.database import DbInfo
-        user = DbInfo.get_db().session.query(User).filter_by(login='admin').first()
+        user = DbInfo.get_db().session.query(User).filter_by(login='super_admin').first()
         role = DbInfo.get_db().session.query(Role).filter_by(name='import_store_data').first()
         user.roles.append(role)
         from app.database import DbInfo