e40f59af8b13151915bbd1fd7ac2bb4c80836ecb
Lawrence <lawrence@betterlife.io>
2016-07-07 22:16:19 +0800

    Refine setup and remove duplicate requirements (BE-280)

--- setup.py
+++ setup.py
@@ -10,33 +10,37 @@ with open('app/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r", "")
+except(IOError, ImportError):
+    import io
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+
 setup(
-    name="Betterlife PSI",
+    name="betterlife-psi",
     version=version,
     packages=['app', 'migrations'],
     include_package_data=True,
     author="Lawrence Liu",
     author_email="lawrence@betterlife.io",
     description="Betterlife Intelligent PSI(Purchase, Sales and Inventory) system",
+    long_description=long_description,
     license="MIT",
-    keywords="Betterlife, Intelligent, Purchase, Sales, Inventory",
+    keywords="Betterlife, Intelligent, Purchase Order, Sales Order, Inventory Management, Retail",
     url="https://github.com/betterlife/psi",
     install_requires=[
         "Flask>=0.11.1",
         "Flask-SQLAlchemy==2.0",
         "Flask-SSLify==0.1.5",
-        "Jinja2==2.7.3",
         "MarkupSafe==0.23",
-        "SQLAlchemy==1.0.4",
-        "Werkzeug==0.10.4",
         "gunicorn==19.3.0",
-        "itsdangerous==0.24",
         "psycopg2==2.6",
         "wsgiref==0.1.2",
-        "Babel==1.3",
         "Flask_BabelEx==0.9.2",
         "Flask-Migrate==1.4.0",
-        "blinker==1.4.0",
         "raven==5.10.2",
         "Flask-Security==1.7.5",
         "Flask-DebugToolbar==0.10.0",
