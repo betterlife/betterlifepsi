ca131b5a318f068b0a7ada3f9a47f0b456a28048
刘向前 <lawrence@betterlife.io>
2016-08-18 01:03:14 +0800

    Add more API doc to public method

--- app/utils/format_util.py
+++ app/utils/format_util.py
@@ -2,4 +2,8 @@ from decimal import Decimal, ROUND_HALF_UP
 
 
 def format_decimal(value):
+    """
+    Format a decimal with two decimal point with rounding mode ROUND_HALF_UP
+    :param value the decimal to format
+    """
     return Decimal(Decimal(value).quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
