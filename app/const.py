betterlife/betterlifepsi

commit b3e5aa01bd8f30384090d9c801733432c2a71b65

Add model migration for BE-258

migrations/versions/27_7868d8cd502d_.py
@@ -1,5 +1,5 @@
 # encoding=utf-8
-"""Add type to organization and franchise price to product
+"""Add seed data, schema change for franchise support
 
 Revision ID: 7868d8cd502d
 Revises: 0051eed6ee5d
@@ -18,6 +18,7 @@ import sqlalchemy as sa
 def upgrade():
     ### commands auto generated by Alembic - please adjust! ###
     from sqlalchemy.sql import text
+    # Organization
     op.add_column('organization', sa.Column('type_id', sa.Integer(), nullable=True))
     op.create_foreign_key(None, 'organization', 'enum_values', ['type_id'], ['id'])
     op.add_column('product', sa.Column('franchise_price', sa.Numeric(precision=8, scale=2, decimal_return_scale=2), nullable=True))
@@ -26,14 +27,51 @@ def upgrade():
     op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) VALUES ((SELECT id FROM enum_values WHERE code='ORGANIZATION_TYPE'), 'FRANCHISE_STORE', '加盟店');"))
     op.get_bind().execute(text("UPDATE organization set type_id = (SELECT id FROM enum_values WHERE code='DIRECT_SELLING_STORE')"))
     op.get_bind().execute(text("ALTER TABLE organization ALTER COLUMN type_id SET NOT NULL;"))
+
+    # Purchase Order
+    op.add_column('purchase_order', sa.Column('type_id', sa.Integer(), nullable=True))
+    op.create_foreign_key(None, 'purchase_order', 'enum_values', ['type_id'], ['id'])
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values (1, 'PURCHASE_ORDER_TYPE', '采购单类型');"))
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values ((SELECT id FROM enum_values WHERE code='PURCHASE_ORDER_TYPE'), 'DIRECT_PURCHASE_ORDER', '直接采购单');"))
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values ((SELECT id FROM enum_values WHERE code='PURCHASE_ORDER_TYPE'), 'FRANCHISE_PURCHASE_ORDER', '加盟商采购单');"))
+    op.get_bind().execute(text("UPDATE purchase_order set type_id = (SELECT id FROM enum_values WHERE code='DIRECT_PURCHASE_ORDER')"))
+    op.get_bind().execute(text("ALTER TABLE purchase_order ALTER COLUMN type_id SET NOT NULL;"))
+
+    # Sales order
+    op.add_column('sales_order', sa.Column('type_id', sa.Integer(), nullable=True))
+    op.create_foreign_key(None, 'sales_order', 'enum_values', ['type_id'], ['id'])
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values (1, 'SALES_ORDER_TYPE', '销售单类型');"))
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values ((SELECT id FROM enum_values WHERE code='SALES_ORDER_TYPE'), 'DIRECT_SALES_ORDER', '直接销售单');"))
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values ((SELECT id FROM enum_values WHERE code='SALES_ORDER_TYPE'), 'FRANCHISE_SALES_ORDER', '加盟商销售单');"))
+    op.get_bind().execute(text("UPDATE sales_order set type_id = (SELECT id FROM enum_values WHERE code='DIRECT_SALES_ORDER')"))
+    op.get_bind().execute(text("ALTER TABLE sales_order ALTER COLUMN type_id SET NOT NULL;"))
+
+    # New inventory transaction type
+    op.get_bind().execute(text("INSERT INTO enum_values (type_id, code, display) values ((SELECT id FROM enum_values WHERE code='INVENTORY_TRANSACTION_TYPE'), 'FRANCHISE_SALES_OUT', '加盟商销售出库');"))
+    op.get_bind().execute(text("UPDATE enum_values set code='DIRECT_SALES_OUT', display='直接销售出库' WHERE code='SALES_OUT'"))
+
+    # New role for manage sales order from FRANCHISE
     ### end Alembic commands ###
 
 
 def downgrade():
     ### commands auto generated by Alembic - please adjust! ###
     from sqlalchemy.sql import text
-    op.get_bind().execute(text("DELETE FROM enum_values where code in ('ORGANIZATION_TYPE', 'DIRECT_SELLING_STORE', 'FRANCHISE_STORE')"))
-    op.drop_column('product', 'franchise_price')
-    op.drop_constraint(None, 'organization', type_='foreignkey')
+    op.get_bind().execute(text("UPDATE enum_values set code='SALES_OUT', display='销售出库' WHERE code='DIRECT_SALES_OUT'"))
+    op.get_bind().execute(text("DELETE FROM enum_values where code = 'FRANCHISE_SALES_OUT'"))
+
+    # Sales Order
+    op.drop_column('sales_order', 'type_id')
+    op.get_bind().execute(text("DELETE FROM enum_values where code in ('SALES_ORDER_TYPE', 'DIRECT_SALES_ORDER', 'FRANCHISE_SALES_ORDER')"))
+
+    # Purchase Order
+    op.drop_column('purchase_order', 'type_id')
+    op.get_bind().execute(text("DELETE FROM enum_values where code in ('PURCHASE_ORDER_TYPE', 'DIRECT_PURCHASE_ORDER', 'FRANCHISE_PURCHASE_ORDER')"))
+
+    # Organization
     op.drop_column('organization', 'type_id')
+    op.get_bind().execute(text("DELETE FROM enum_values where code in ('ORGANIZATION_TYPE', 'DIRECT_SELLING_STORE', 'FRANCHISE_STORE')"))
+
+    # Drop franchise_price field
+    op.drop_column('product', 'franchise_price')
     ### end Alembic commands ###

app/models/purchase_order.py
@@ -23,6 +23,9 @@ class PurchaseOrder(db.Model, DataSecurityMixin):
     status_id = Column(Integer, ForeignKey('enum_values.id'), nullable=False)
     status = relationship('EnumValues', foreign_keys=[status_id])
 
+    type_id = Column(Integer, ForeignKey('enum_values.id'), nullable=False)
+    type = relationship('EnumValues', foreign_keys=[type_id])
+
     organization_id = db.Column(Integer, ForeignKey('organization.id'))
     organization = relationship('Organization', foreign_keys=[organization_id])
 

app/models/sales_order.py
@@ -24,6 +24,9 @@ class SalesOrder(db.Model, DataSecurityMixin):
     organization_id = db.Column(Integer, ForeignKey('organization.id'))
     organization = relationship('Organization', foreign_keys=[organization_id])
 
+    type_id = Column(Integer, ForeignKey('enum_values.id'), nullable=False)
+    type = relationship('EnumValues', foreign_keys=[type_id])
+
     remark = Column(Text)
 
     @hybrid_property

commit 75e8fb18ca7cda1909db8cb1dc4ccb4c4d8ed6f5

Fix regression test error caused by b3e5aa01bd8 (BE-258)

app/const.py
@@ -5,9 +5,17 @@ PO_PART_RECEIVED_STATUS_KEY = u'PURCHASE_ORDER_PART_RECEIVED'
 PO_DRAFT_STATUS_KEY = u'PURCHASE_ORDER_DRAFT'
 PO_ISSUED_STATUS_KEY = u'PURCHASE_ORDER_ISSUED'
 
+# PO type key
+PO_TYPE_KEY = u'PURCHASE_ORDER_TYPE'
+DIRECT_PO_TYPE_KEY = u'DIRECT_PURCHASE_ORDER'
+
+# SO type key
+SO_TYPE_KEY = u'SALES_ORDER_TYPE'
+DIRECT_SO_TYPE_KEY = u'DIRECT_SALES_ORDER'
+
 # Inventory transaction type definition key
 PURCHASE_IN_INV_TRANS_KEY = u'PURCHASE_IN'
-SALES_OUT_INV_TRANS_TYPE_KEY = u'SALES_OUT'
+SALES_OUT_INV_TRANS_TYPE_KEY = u'DIRECT_SALES_OUT'
 
 # Inventory transaction type definition
 INVENTORY_TRANSACTION_TYPE_KEY = u'INVENTORY_TRANSACTION_TYPE'

app/views/import_store_data.py
@@ -61,6 +61,7 @@ def create_or_update_sales_order(po_num, po_line_num, product, act_price, qty, s
         order.logistic_amount = 0
         order.organization_id = current_user.organization_id
     order.order_date = sale_date
+    order.type = EnumValues.find_one_by_code(const.DIRECT_SO_TYPE_KEY)
     existing = False
     line = None
     for line in order.lines:

migrations/versions/27_7868d8cd502d_.py
@@ -51,24 +51,41 @@ def upgrade():
     op.get_bind().execute(text("UPDATE enum_values set code='DIRECT_SALES_OUT', display='直接销售出库' WHERE code='SALES_OUT'"))
 
     # New role for manage sales order from FRANCHISE
+    #
+    op.get_bind().execute(text("INSERT INTO role (name, description) VALUES ('franchise_sales_order_create', 'Franchise Sales Order Create');"))
+    op.get_bind().execute(text("INSERT INTO role (name, description) VALUES ('franchise_sales_order_view', 'Franchise Sales Order View');"))
+    op.get_bind().execute(text("INSERT INTO role (name, description) VALUES ('franchise_sales_order_edit', 'Franchise Sales Order Edit');"))
+    op.get_bind().execute(text("INSERT INTO role (name, description) VALUES ('franchise_sales_order_delete', 'Franchise Sales Order Delete');"))
+    op.get_bind().execute(text("UPDATE role set name='direct_sales_order_create' where name='sales_order_create'"))
+    op.get_bind().execute(text("UPDATE role set name='direct_sales_order_view' where name='sales_order_view'"))
+    op.get_bind().execute(text("UPDATE role set name='direct_sales_order_edit' where name='sales_order_edit'"))
+    op.get_bind().execute(text("UPDATE role set name='direct_sales_order_delete' where name='sales_order_delete'"))
     ### end Alembic commands ###
-
 
 def downgrade():
     ### commands auto generated by Alembic - please adjust! ###
     from sqlalchemy.sql import text
+
+    # Revert Related roles for franchise support
+    op.get_bind().execute(text("UPDATE role set name='sales_order_create' where name='direct_sales_order_create'"))
+    op.get_bind().execute(text("UPDATE role set name='sales_order_view'   where name='direct_sales_order_view'"))
+    op.get_bind().execute(text("UPDATE role set name='sales_order_edit'   where name='direct_sales_order_edit'"))
+    op.get_bind().execute(text("UPDATE role set name='sales_order_delete' where name='direct_sales_order_delete'"))
+    op.get_bind().execute(text("DELETE FROM role where name in ('franchise_sales_order_create', 'franchise_sales_order_view', 'franchise_sales_order_edit', 'franchise_sales_order_delete')"))
+
+    # Revert Inventory transaction support for franchise support
     op.get_bind().execute(text("UPDATE enum_values set code='SALES_OUT', display='销售出库' WHERE code='DIRECT_SALES_OUT'"))
     op.get_bind().execute(text("DELETE FROM enum_values where code = 'FRANCHISE_SALES_OUT'"))
 
-    # Sales Order
+    # Delete Sales Order type
     op.drop_column('sales_order', 'type_id')
     op.get_bind().execute(text("DELETE FROM enum_values where code in ('SALES_ORDER_TYPE', 'DIRECT_SALES_ORDER', 'FRANCHISE_SALES_ORDER')"))
 
-    # Purchase Order
+    # Delete Purchase Order type
     op.drop_column('purchase_order', 'type_id')
     op.get_bind().execute(text("DELETE FROM enum_values where code in ('PURCHASE_ORDER_TYPE', 'DIRECT_PURCHASE_ORDER', 'FRANCHISE_PURCHASE_ORDER')"))
 
-    # Organization
+    # delete Organization type
     op.drop_column('organization', 'type_id')
     op.get_bind().execute(text("DELETE FROM enum_values where code in ('ORGANIZATION_TYPE', 'DIRECT_SELLING_STORE', 'FRANCHISE_STORE')"))
 

tests/object_faker.py
@@ -18,6 +18,7 @@ class ObjectFaker(object):
         po.logistic_amount = self.faker.pyfloat(positive=True, left_digits=2, right_digits=0)
         po.order_date = self.faker.date()
         po.status = EnumValues.find_one_by_code(const.PO_DRAFT_STATUS_KEY)
+        po.type = EnumValues.find_one_by_code(const.DIRECT_PO_TYPE_KEY)
         po.id = po_id if po_id is not None else db_util.get_next_id(PurchaseOrder)
         po.organization = creator.organization
         po.supplier = self.supplier()