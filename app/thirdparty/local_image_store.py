99bba0855d122141fda506b6e0bf127aab968823
Liu Xiangqian <lawrence@betterlife.io>
2016-12-26 22:13:56 +0800

    Implemented a local image store service and enable it for local development by default.

--- app/thirdparty/local_image_store.py
+++ app/thirdparty/local_image_store.py
@@ -1,11 +1,43 @@
-class ImageStore(object):
+import os
+
+import errno
+
+from flask import current_app as app
+
+
+class LocalImageStore(object):
+    """
+    A local image store service
+    Images will be saved on static/uploaded folder
+    """
     def __init__(self, app):
         pass
 
     @staticmethod
     def save(image_file, public_id):
-       pass
+        file_sep = '.'
+        if (file_sep in image_file.filename
+            and image_file.filename.rsplit(file_sep, 1)[1].lower() in app.config['IMAGE_ALLOWED_EXTENSIONS']):
+            extension = image_file.filename.rsplit(file_sep, 1)[1].lower()
+            filename = "{0}.{1}".format(public_id, extension)
+            file_path_all = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(file_path_all)
+            result = {'url': "/static/uploaded/" + filename}
+        else:
+            result = {}
+        return result
 
     @staticmethod
-    def remove(public_id):
-        pass
+    def remove(path):
+        """
+           Remove a file from operation system and omit the error if it's not existing.
+           :param filename: name of the file
+           :return: None
+           """
+        try:
+            os.remove(path)
+        except OSError as e:
+            # errno.ENOENT = no such file or directory
+            if e.errno != errno.ENOENT:
+                # re-raise exception if a different error occurred
+                raise
