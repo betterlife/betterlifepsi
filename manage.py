78c0a9bf8086538361987501feb2a35da027f1b9
Liu Xiangqian <lawrence@betterlife.io>
2017-01-06 22:14:51 +0800

    Fixes BE-301(Transaction data clean up command)

--- manage.py
+++ manage.py
@@ -62,6 +62,140 @@ def generate_fake_order():
         database.session.add(sales_order)
     database.session.commit()
 
+
+@manager.command
+def clean_transaction_data():
+    """
+    Clean all the transaction data, and keep all master data
+    """
+    database.engine.execute("""
+        DELETE FROM related_values;
+        DELETE FROM incoming;
+        DELETE FROM shipping_line;
+        DELETE FROM shipping;
+        DELETE FROM expense;
+        DELETE FROM receiving_line;
+        DELETE FROM receiving;
+        DELETE FROM purchase_order_line;
+        DELETE FROM purchase_order;
+        DELETE FROM sales_order_line;
+        DELETE FROM sales_order;
+        DELETE FROM inventory_transaction_line;
+        DELETE FROM inventory_transaction;
+        commit;
+    """)
+
+@manager.command
+def clean_database():
+    """
+    Clean the database and drop all the tables
+    This only tested for postgresql at this moment
+    """
+    database.engine.execute("""
+        DROP VIEW sales_order_detail RESTRICT;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_so_incoming_type_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_so_incoming_status_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_so_exp_type_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_so_exp_status_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_po_logistic_exp_status_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_po_logistic_exp_type_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_po_goods_exp_status_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_def_po_goods_exp_type_id_fkey;
+        ALTER TABLE preference DROP CONSTRAINT preference_organization_id_fkey;
+        ALTER TABLE related_values DROP CONSTRAINT related_values_relation_type_id_fkey;
+        ALTER TABLE incoming DROP CONSTRAINT incoming_category_id_fkey;
+        ALTER TABLE incoming DROP CONSTRAINT incoming_status_id_fkey;
+        ALTER TABLE incoming DROP CONSTRAINT incoming_sales_order_id_fkey;
+        ALTER TABLE incoming DROP CONSTRAINT incoming_organization_id_fkey;
+        ALTER TABLE shipping_line DROP CONSTRAINT shipping_line_product_id_fkey;
+        ALTER TABLE shipping_line DROP CONSTRAINT shipping_line_shipping_id_fkey;
+        ALTER TABLE shipping_line DROP CONSTRAINT shipping_line_sales_order_line_id_fkey;
+        ALTER TABLE shipping_line DROP CONSTRAINT shipping_line_inventory_transaction_line_id_fkey;
+        ALTER TABLE payment_method DROP CONSTRAINT payment_method_supplier_id_fkey;
+        ALTER TABLE expense DROP CONSTRAINT expense_status_id_fkey;
+        ALTER TABLE expense DROP CONSTRAINT expense_category_id_fkey;
+        ALTER TABLE expense DROP CONSTRAINT expense_purchase_order_id_fkey;
+        ALTER TABLE expense DROP CONSTRAINT expense_sales_order_id_fkey;
+        ALTER TABLE expense DROP CONSTRAINT expense_organization_id_fkey;
+        ALTER TABLE roles_users DROP CONSTRAINT roles_users_user_id_fkey;
+        ALTER TABLE roles_users DROP CONSTRAINT roles_users_role_id_fkey;
+        ALTER TABLE product_image DROP CONSTRAINT product_image_product_id_fkey;
+        ALTER TABLE product_image DROP CONSTRAINT product_image_image_id_fkey;
+        ALTER TABLE receiving_line DROP CONSTRAINT receiving_line_product_id_fkey;
+        ALTER TABLE receiving_line DROP CONSTRAINT receiving_line_receiving_id_fkey;
+        ALTER TABLE receiving_line DROP CONSTRAINT receiving_line_purchase_order_line_id_fkey;
+        ALTER TABLE receiving_line DROP CONSTRAINT receiving_line_inventory_transaction_line_id_fkey;
+        ALTER TABLE shipping DROP CONSTRAINT shipping_status_id_fkey;
+        ALTER TABLE shipping DROP CONSTRAINT shipping_sales_order_id_fkey;
+        ALTER TABLE shipping DROP CONSTRAINT shipping_inventory_transaction_id_fkey;
+        ALTER TABLE shipping DROP CONSTRAINT shipping_organization_id_fkey;
+        ALTER TABLE shipping DROP CONSTRAINT shipping_type_id_fkey;
+        ALTER TABLE purchase_order DROP CONSTRAINT purchase_order_supplier_id_fkey;
+        ALTER TABLE purchase_order DROP CONSTRAINT purchase_order_status_id_fkey;
+        ALTER TABLE purchase_order DROP CONSTRAINT purchase_order_organization_id_fkey;
+        ALTER TABLE purchase_order DROP CONSTRAINT purchase_order_type_id_fkey;
+        ALTER TABLE purchase_order DROP CONSTRAINT to_organization_id_fkey;
+        ALTER TABLE sales_order DROP CONSTRAINT sales_order_customer_id_fkey;
+        ALTER TABLE sales_order DROP CONSTRAINT sales_order_organization_id_fkey;
+        ALTER TABLE sales_order DROP CONSTRAINT sales_order_type_id_fkey;
+        ALTER TABLE sales_order DROP CONSTRAINT sales_order_status_id_fkey;
+        ALTER TABLE receiving DROP CONSTRAINT receiving_status_id_fkey;
+        ALTER TABLE receiving DROP CONSTRAINT receiving_purchase_order_id_fkey;
+        ALTER TABLE receiving DROP CONSTRAINT receiving_inventory_transaction_id_fkey;
+        ALTER TABLE receiving DROP CONSTRAINT receiving_organization_id_fkey;
+        ALTER TABLE "user" DROP CONSTRAINT user_locale_id_fkey;
+        ALTER TABLE "user" DROP CONSTRAINT user_timezone_id_fkey;
+        ALTER TABLE "user" DROP CONSTRAINT user_organization_id_fkey;
+        ALTER TABLE product DROP CONSTRAINT product_category_id_fkey;
+        ALTER TABLE product DROP CONSTRAINT product_supplier_id_fkey;
+        ALTER TABLE product DROP CONSTRAINT product_organization_id_fkey;
+        ALTER TABLE customer DROP CONSTRAINT customer_join_channel_id_fkey;
+        ALTER TABLE customer DROP CONSTRAINT customer_level_id_fkey;
+        ALTER TABLE customer DROP CONSTRAINT customer_organization_id_fkey;
+        ALTER TABLE purchase_order_line DROP CONSTRAINT purchase_order_line_purchase_order_id_fkey;
+        ALTER TABLE purchase_order_line DROP CONSTRAINT purchase_order_line_product_id_fkey;
+        ALTER TABLE product_category DROP CONSTRAINT product_category_parent_id_fkey;
+        ALTER TABLE product_category DROP CONSTRAINT product_category_organization_id_fkey;
+        ALTER TABLE inventory_transaction DROP CONSTRAINT inventory_transaction_type_id_fkey;
+        ALTER TABLE inventory_transaction DROP CONSTRAINT inventory_transaction_organization_id_fkey;
+        ALTER TABLE inventory_transaction_line DROP CONSTRAINT inventory_transaction_line_product_id_fkey;
+        ALTER TABLE inventory_transaction_line DROP CONSTRAINT inventory_transaction_line_inventory_transaction_id_fkey;
+        ALTER TABLE sales_order_line DROP CONSTRAINT sales_order_line_sales_order_id_fkey;
+        ALTER TABLE sales_order_line DROP CONSTRAINT sales_order_line_product_id_fkey;
+        ALTER TABLE supplier DROP CONSTRAINT supplier_organization_id_fkey;
+        ALTER TABLE enum_values DROP CONSTRAINT enum_values_type_id_fkey;
+        ALTER TABLE organization DROP CONSTRAINT organization_type_id_fkey;
+        ALTER TABLE role DROP CONSTRAINT role_parent_id_fkey;
+        DROP TABLE preference;
+        DROP TABLE related_values;
+        DROP TABLE incoming;
+        DROP TABLE shipping_line;
+        DROP TABLE payment_method;
+        DROP TABLE expense;
+        DROP TABLE roles_users;
+        DROP TABLE product_image;
+        DROP TABLE receiving_line;
+        DROP TABLE shipping;
+        DROP TABLE purchase_order;
+        DROP TABLE sales_order;
+        DROP TABLE receiving;
+        DROP TABLE "user";
+        DROP TABLE product;
+        DROP TABLE customer;
+        DROP TABLE purchase_order_line;
+        DROP TABLE product_category;
+        DROP TABLE inventory_transaction;
+        DROP TABLE inventory_transaction_line;
+        DROP TABLE sales_order_line;
+        DROP TABLE supplier;
+        DROP TABLE enum_values;
+        DROP TABLE organization;
+        DROP TABLE role;
+        DROP TABLE image;
+        DROP TABLE alembic_version;
+        commit;
+    """)
+
 @application.teardown_appcontext
 def shutdown_session(exception=None):
     database.session.remove()
