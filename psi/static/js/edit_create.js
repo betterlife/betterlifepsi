c222982fd489f749847c63a019e73e984c043757
Liu Xiangqian <lawrence@betterlife.io>
2017-04-16 23:13:22 +0800

    Fixes BE-275, Add max header panel and minimal lines panel.

--- psi/static/js/edit_create.js
+++ psi/static/js/edit_create.js
@@ -9,31 +9,65 @@ $(document).ready(function () {
         elem.addClass(newCls);
     }
 
-    function switch_inline_field_panel_display(switch_function) {
-        var inline = $("#inline-field-panel"),
-            header_fields_panel = $("#header-field-panel"),
-            expand_header_panel_icon = $("#header-field-panel-expand-icon"),
-            submit_row = $(".submit-row");
-        header_fields_panel.toggle();
-        switch_function(inline, submit_row);
-        expand_header_panel_icon.toggle();
+    function moveSubmitRowToCenter(submit_row) {
+        switchClasses(submit_row, 'col-md-offset-6 col-md-6', 'col-md-offset-4 col-md-8');
     }
 
-   $("#header-field-panel-collapse-icon").click(function () {
-       function expand_inline_fields_panel(inline, submit_row) {
-           switchClasses(inline, "col-md-8", "expand-inline-field-panel");
-           switchClasses(submit_row, 'col-md-offset-6 col-md-6', 'col-md-offset-4 col-md-8');
-       }
-       switch_inline_field_panel_display(expand_inline_fields_panel);
+    function moveSubmitRowToRight(submit_row) {
+        switchClasses(submit_row, 'col-md-offset-4 col-md-8', 'col-md-offset-6 col-md-6');
+    }
+
+    $("#header-field-panel-collapse-icon").click(function () {
+       var inline = $("#inline-field-panel"),
+           header = $("#header-field-panel"),
+           expand_header_panel_icon = $("#header-field-panel-expand-icon"),
+           submit_row = $(".submit-row");
+       header.toggle();
+       switchClasses(inline, "col-md-8", "expand-inline-field-panel");
+       moveSubmitRowToCenter(submit_row);
+       expand_header_panel_icon.toggle();
    });
 
     $("#header-field-panel-expand-icon").click(function () {
-        function collapse_inline_fields_panel(inline, submit_row) {
-            switchClasses(inline,  "expand-inline-field-panel","col-md-8");
-            switchClasses(submit_row, 'col-md-offset-4 col-md-8', 'col-md-offset-6 col-md-6');
-        }
-        switch_inline_field_panel_display(collapse_inline_fields_panel);
-   })
+        var inline = $("#inline-field-panel"),
+            header = $("#header-field-panel"),
+            expand_header_panel_icon = $("#header-field-panel-expand-icon"),
+            submit_row = $(".submit-row");
+        header.toggle();
+        switchClasses(inline,  "expand-inline-field-panel","col-md-8");
+        moveSubmitRowToRight(submit_row);
+        expand_header_panel_icon.toggle();
+   });
+
+    $("#inline-field-panel-collapse-icon").click(function () {
+        var inline = $("#inline-field-panel"),
+            header = $("#header-field-panel"),
+            expand_inline_panel_icon = $("#inline-field-panel-expand-icon"),
+            collapse_inline_panel_icon = $("#inline-field-panel-collapse-icon"),
+            collapse_header_panel_icon = $("#header-field-panel-collapse-icon"),
+            submit_row = $(".submit-row");
+        inline.toggle();
+        switchClasses(header, 'col-md-4', 'col-md-12');
+        moveSubmitRowToCenter(submit_row);
+        expand_inline_panel_icon.toggle();
+        collapse_inline_panel_icon.toggle();
+        collapse_header_panel_icon.toggle();
+    });
+
+    $("#inline-field-panel-expand-icon").click(function () {
+        var inline = $("#inline-field-panel"),
+            header = $("#header-field-panel"),
+            expand_inline_panel_icon = $("#inline-field-panel-expand-icon"),
+            collapse_inline_panel_icon = $("#inline-field-panel-collapse-icon"),
+            collapse_header_panel_icon = $("#header-field-panel-collapse-icon"),
+            submit_row = $(".submit-row");
+        inline.toggle();
+        switchClasses(header, 'col-md-12', 'col-md-4');
+        moveSubmitRowToRight(submit_row);
+        expand_inline_panel_icon.toggle();
+        collapse_inline_panel_icon.toggle();
+        collapse_header_panel_icon.toggle();
+    })
 });
 
 function addInlineField(parent_id) {
@@ -62,6 +96,9 @@ function addInlineField(parent_id) {
 
         me.attr('id', id);
         me.attr('name', name);
+        if (me.attr('class') === 'line-number') {
+            me.text(prefix);
+        }
     });
 
     $template.appendTo(parent);
