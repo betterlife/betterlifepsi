46b8200f9ca2e018eab7c4e5826f80f6ed29a5ca
Liu Xiangqian <lawrence@betterlife.io>
2017-04-17 00:30:43 +0800

    Fixes BE-282 Display line number for newly added lines

--- psi/static/js/edit_create.js
+++ psi/static/js/edit_create.js
@@ -70,10 +70,25 @@ $(document).ready(function () {
     })
 });
 
+// When user clicks remove inline field icon
+// Reorder the inline orders and recalculate line number cell.
+$('body').on('click', '.inline-remove-field' , function(e) {
+    var inline_lines_tr = parent.find(".inline-field");
+    for (i = 0; i < inline_lines_tr.length; i++){
+        var line = $(inline_lines_tr[i]);
+        if (line.attr("class").indexOf('fresh') > 0) {
+            $('.line-number', line).each(function (e1) {
+                var me = $(this);
+                me.html(i+ 1);
+            });
+        }
+    }
+});
+
 function addInlineField(parent_id) {
     parent = $("#" + parent_id);
-    $trs = parent.find(".inline-field");
-    var prefix = parent_id + '-' + $trs.length;
+    inline_lines_tr = parent.find(".inline-field");
+    var prefix = parent_id + '-' + inline_lines_tr.length;
 
     // Get template
     var $template = $($('.inline-field-template-' + parent_id).text());
@@ -84,6 +99,11 @@ function addInlineField(parent_id) {
     // Mark form that we just created
     $template.addClass('fresh');
     $template.removeClass('hide');
+    // Display line number for newly added line.
+    $('.line-number', $template).each(function(e) {
+        var me = $(this);
+        me.html(inline_lines_tr.length + 1);
+    });
     // Fix form IDs
     $('[name]', $template).each(function (e) {
         var me = $(this);
