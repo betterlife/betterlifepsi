e0e87977a887bc19574372f771e766cd6b2b2199
xqliu <xqliu.action@gmail.com>
2017-06-20 15:16:15 +0000

    Fix swtag information on bitbucket

--- psi/app/utils/ui_util.py
+++ psi/app/utils/ui_util.py
@@ -71,7 +71,7 @@ def render_version(swtag_file=default_swtag_file):
         if tag == '':
             tag = 'None'
         commit = commit[:7]
-        build_url = "{builder_url}/{b_id}".format(builder_url=builder_url, b_id=b_id)
+        build_url = "{builder_url}/{commit}".format(builder_url=builder_url, commit=commit)
         b_link = '<a href="{build_url}" target="_blank">{b_num}</a>'.format(build_url=build_url, b_num=b_num)
         commit_url = "{git_url}/{commit}".format(git_url=git_url, commit=commit)
         commit_link = '<a href="{commit_url}" target="_blank">{commit}</a>'.format(commit_url=commit_url, commit=commit)
