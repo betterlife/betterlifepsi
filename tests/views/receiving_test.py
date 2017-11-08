53428d0e7d2abfa76d240157965fc5ffe4a4d753
Liu Xiangqian <lawrence@betterlife.io>
2017-11-08 12:44:17 +0800

    Fix test failure and add codecov report upon test run

--- tests/views/receiving_test.py
+++ tests/views/receiving_test.py
@@ -3,30 +3,31 @@ from flask_admin.consts import ICON_TYPE_GLYPH
 from flask_babelex import lazy_gettext
 from wtforms import ValidationError
 
-from fixture import run_as_admin
-from object_faker import object_faker
+from tests.fixture import run_as_admin
+from tests.object_faker import object_faker
 from psi.app.const import DIRECT_PO_TYPE_KEY, PO_ISSUED_STATUS_KEY, \
     RECEIVING_DRAFT_STATUS_KEY, RECEIVING_COMPLETE_STATUS_KEY
-from tests import fixture
 from tests.base_test_case import BaseTestCase
 
 
 class TestReceivingAdmin(BaseTestCase):
 
     def test_delete_complete_receiving_not_allowed(self):
-        from psi.app.models import Receiving, EnumValues
-        from psi.app.views import ReceivingAdmin
-        from psi.app.service import Info
-        fixture.login_as_admin(self.test_client)
-        receiving = Receiving()
-        complete_status = EnumValues.get(RECEIVING_COMPLETE_STATUS_KEY)
-        receiving.status = complete_status
-        db_session = Info.get_db().session
-        receiving_admin = ReceivingAdmin(Receiving, db_session, name=lazy_gettext("Receiving"),
-                                         category=lazy_gettext('Purchase'), menu_icon_type=ICON_TYPE_GLYPH,
-                                         menu_icon_value='glyphicon-import')
+        def test_logic():
+            from psi.app.models import Receiving, EnumValues
+            from psi.app.views import ReceivingAdmin
+            from psi.app.service import Info
+            receiving = Receiving()
+            complete_status = EnumValues.get(RECEIVING_COMPLETE_STATUS_KEY)
+            receiving.status = complete_status
+            db_session = Info.get_db().session
+            receiving_admin = ReceivingAdmin(Receiving, db_session, name=lazy_gettext("Receiving"),
+                                             category=lazy_gettext('Purchase'), menu_icon_type=ICON_TYPE_GLYPH,
+                                             menu_icon_value='glyphicon-import')
 
-        self.assertRaises(ValidationError, receiving_admin.on_model_delete, receiving)
+            self.assertRaises(ValidationError, receiving_admin.on_model_delete, receiving)
+
+        run_as_admin(self.test_client, test_logic)
 
     def test_receiving_pages(self):
         from psi.app.models.enum_values import EnumValues
