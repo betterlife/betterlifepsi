079f582e89706518eacc9f8972db3a22186091e7
Lawrence Liu <lawrence@betterlife.io>
2018-02-05 09:37:41 +0800

    Fixes BE362 Error msg shown when open receiving document detail page.

--- psi/app/views/base.py
+++ psi/app/views/base.py
@@ -148,6 +148,7 @@ class ModelViewWithAccess(ModelView):
         """
         model, return_url = self.get_model_return_url()
         if not model.can_edit():
+            flash(gettext('You are not allowed to edit this object'))
             return redirect(return_url)
         return super(ModelViewWithAccess, self).edit_view()
 
@@ -159,6 +160,7 @@ class ModelViewWithAccess(ModelView):
         """
         model, return_url = self.get_model_return_url()
         if not model.can_view_details():
+            flash(gettext('You are not allowed to view detail of this object'))
             return redirect(return_url)
         return super(ModelViewWithAccess, self).details_view()
 
@@ -176,6 +178,7 @@ class ModelViewWithAccess(ModelView):
             model_id = form.id.data
             model = self.get_one(model_id)
             if not model.can_delete():
+                flash(gettext('You are not allowed to delete this object'))
                 return redirect(return_url)
         return super(ModelViewWithAccess, self).delete_view()
 
