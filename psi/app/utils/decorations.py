30a0032427df8e5f7ae58fb435e0111803f821c2
Ben Lewis <ben@keep.com>
2018-10-22 16:49:35 -0400

    Adding Docker files + compose and some scripts

--- psi/app/utils/decorations.py
+++ psi/app/utils/decorations.py
@@ -1,9 +1,17 @@
 # encoding=utf-8
+import itertools
+import logging
+import math
+import random
+import time
 from functools import wraps
 
 from flask_login import current_user
 
 from psi.app.utils.security_util import default_action_on_error
+
+
+log = logging.getLogger(__name__)
 
 
 def has_role(expect_role, action_on_error=default_action_on_error):
@@ -27,3 +35,40 @@ def has_role(expect_role, action_on_error=default_action_on_error):
         return decorated_function
 
     return decorator
+
+
+def retry(times, timeout_scaler=0.2, exceptions=None):
+    """A retry decorator. Exponential timeout plus some jitter
+
+    :param times: integer number of times to retry
+    :param timeout_scaler: float number of seconds to wait between calls
+    :param exceptions: tuple of exceptions to retry on. defaults to all
+    """
+    if exceptions is None:
+        exceptions = (Exception,)
+    else:
+        exceptions = tuple(exceptions)
+
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated(*args, **kwargs):
+            for attempt in itertools.count(1):
+                exponential_time = math.pow(2, attempt - 1)
+                jitter = random.uniform(0.75, 1.5)
+
+                timeout = max(timeout_scaler * exponential_time * jitter, 0)
+                try:
+                    if attempt > 1:
+                        log.info("Calling function '%s' attempt '%s'",
+                                 fn.__name__, attempt)
+                    else:
+                        log.debug("Calling function '%s' attempt '%s'",
+                                  fn.__name__, attempt)
+                    return fn(*args, **kwargs)
+                except exceptions:
+                    if attempt > times:
+                        raise
+                if timeout:
+                    time.sleep(timeout)
+        return decorated
+    return wrapper
